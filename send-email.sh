8f904f1f3668f8ef1bbf9db9839112ef76d3fa7a
Eli Bates <eli@bateswebtech.com>
2022-06-28 15:20:40 +0200

    add my email as a tester

--- send-email.sh
+++ send-email.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 TEMPLATE=$1
-aws ses send-templated-email --source mail@runday.io --destination ToAddresses=dev@pulsewavelabs.com --template runday-$TEMPLATE --template-data file://./templates/$TEMPLATE/$TEMPLATE.test.json
+aws ses send-templated-email --source mail@runday.io --destination ToAddresses=dev@pulsewavelabs.com,eli.m.bates@gmail.com --template runday-$TEMPLATE --template-data file://./templates/$TEMPLATE/$TEMPLATE.test.json
